7007eeb7480e6c03a84d681d1991dfbd32f2380f
Chaitanya Zarekar <vaishnavi.munde24@mmit.edu.in>
2025-07-17 12:16:39 +0530

    text created

---
+++ Artificial_Intelligence_Lab/decsion.py
@@ -0,0 +1,10 @@
+print("select any two prefered subject :\n1.maths\n2.programming\n3.chemistry\n4.physics\n5.biology\n6.electrical\n7.Statistics\n8.circuit\n9.AI concept")
+sub1=int(input("enter the first preffred subject number:"))
+sub2=int(input("enter the second preffred subject number:"))
+if sub1==1 and sub2==2 :
+    print("suggested carrer:computer engineering")
+if sub1==1 and sub2==4 :
+    print("suggest carrer:Mechanical engineering")
+if sub1==2 and sub2==7 :
+    print("suggest carrer:AIDS engineering")
+ 
